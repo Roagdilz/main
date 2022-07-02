e0c9205ed8832aa34b31ef37f054ce821d5f5dc1
Roagdilz <diazlopezrodrigo14@gmail.com>
2022-07-02 16:37:58 -0300

    checkpoint commit

---
+++ 01.js
@@ -0,0 +1,18 @@
+/* 
+  Importante: 
+  No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
+  código dentro de las funciones ya definidas. 
+  No comentar la funcion 
+*/
+function mayorMenosMenor(arr) {
+  // Obtener del array "arr" el número mayor y el menor. Devuelve la resta entre los mismos
+  // NOTA: No utilizar los métodos "min" y "max" de "Math".
+  //
+  // Tu código:
+  x= arr.reverse()[0] - arr.sort()[0]
+  return x
+};
+
+// No modifiques nada debajo de esta linea //
+
+module.exports = mayorMenosMenor
